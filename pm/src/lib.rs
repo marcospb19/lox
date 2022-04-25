b4864a295b333c889967645f7c391e34539d1e4c
Eva Pace <eba.pachi@gmail.com>
2022-04-25 12:01:42 -0300

    pm: Use quote instead of literal strings

--- pm/src/lib.rs
+++ pm/src/lib.rs
@@ -1,22 +1,23 @@
 use proc_macro::TokenStream;
+use quote::quote;
 
 #[proc_macro]
 pub fn make_expressions(_item: TokenStream) -> TokenStream {
-    let base = "
-// #![cfg_attr(not(test), allow(unused))]
+    let base = quote! {
+        // #![cfg_attr(not(test), allow(unused))]
+        use std::fmt;
 
-use std::fmt;
+        use crate::token::Token;
 
-use crate::token::Token;
+        pub trait Expression: fmt::Display {}
 
-pub trait Expression: fmt::Display {}
+        impl Expression for BinaryExpression {}
+        impl Expression for GroupingExpression {}
+        impl Expression for LiteralExpression {}
+        impl Expression for UnaryExpression {}
 
-impl Expression for BinaryExpression {}
-impl Expression for GroupingExpression {}
-impl Expression for LiteralExpression {}
-impl Expression for UnaryExpression {}
+        pub type ExpressionBox = Box<dyn Expression>;
+    };
 
-pub type ExpressionBox = Box<dyn Expression>;
-";
-base.parse().unwrap()
+    base.into()
 }
